be83209995cd1cdda4ed83f0a57b087fba6412ed
tharun5778 <57627667+tharun5778@users.noreply.github.com>
2020-10-04 23:33:51 +0530

    build error resolved

--- src/components/route.js
+++ src/components/route.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Home from './Home';
+import Home from './home';
 import Main from './main';
 import Loading from './loading';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
