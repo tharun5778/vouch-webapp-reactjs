6eb53820fa6e518bc2b107201db46a9f6ba559d2
tharun5778 <57627667+tharun5778@users.noreply.github.com>
2020-10-04 21:58:38 +0530

    Update main.js

--- src/components/main.js
+++ src/components/main.js
@@ -1,38 +1,48 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import { Button} from 'reactstrap';
+import { Button } from 'reactstrap';
 import './main.css';
 
-class Login extends Component{
-    constructor(props){
+class Login extends Component {
+    constructor(props) {
         super(props);
-        this.state={
-            auth : {},
+        this.state = {
+            auth: {},
             redirect_link: '',
         };
         this.getTwitterToken.bind(this);
     }
-    componentDidMount=()=>{
+    componentDidMount = () => {
         this.getTwitterToken()
     }
-    getTwitterToken=()=>{ 
-    fetch("https://vouch-api-1.herokuapp.com/auth")
-  .then(response => response.json())
-  .then(result => {
-      this.setState({auth:result,redirect_link:"https://api.twitter.com/oauth/authenticate?oauth_token="+result["oauth_token"]+"&force_login=true"})
-    })
-  .catch(error => console.log('error', error));
+    getTwitterToken = () => {
+        fetch("https://vouch-api-1.herokuapp.com/auth")
+            .then(response => response.json())
+            .then(result => {
+                if (!result.error) {
+                    this.setState({ auth: result, redirect_link: "https://api.twitter.com/oauth/authenticate?oauth_token=" + result["oauth_token"] + "&force_login=true" })
+                } else {
+                    alert("internal server error")
+                }
+            })
+            .catch(error => console.log('error', error));
     }
 
-    render(){
+    render() {
         console.log(this.state.auth);
-        return(<div className="background">
-            <div>
-                <Button style={{height:50,width:200,paddingTop:10}} href={this.state.redirect_link}>Login with Twitter</Button>                
-            </div>
-            
-        </div>)
-    }
-}
+        return ( < div className = "background" >
+            <
+            div >
+            <
+            Button style = {
+                { height: 50, width: 200, paddingTop: 10 }
+            }
+            href = { this.state.redirect_link } > Login with Twitter < /Button>                 < /
+            div >
 
-export default Login;
+            <
+            /div>)
+        }
+    }
+
+    export default Login;
