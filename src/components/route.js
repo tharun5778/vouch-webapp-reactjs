a4000c7e41b52fa53aa0f030d784526903c9ef10
tharun5778 <57627667+tharun5778@users.noreply.github.com>
2020-10-04 20:41:23 +0530

    Update route.js

--- src/components/route.js
+++ src/components/route.js
@@ -1,20 +1,29 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Home from './home';
 import Main from './main';
 import Loading from './loading';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-class Routes extends Component{
-    render(){
-        return(<div>
-            <Router>
-                <div className="App">
-                    <Route path={["/main","/"]} component={Main}/>
-                    <Route path="/loading" component={Loading}/>
-                    <Route path="/home" component={Home}/>
-                </div>
-            </Router>
-        </div>);
+class Routes extends Component {
+    render() {
+        return ( < div >
+            <
+            Router >
+            <
+            div className = "App" >
+            <
+            Route path = "/main"
+            component = { Main }
+            /> <
+            Route path = "/loading"
+            component = { Loading }
+            /> <
+            Route path = "/home"
+            component = { Home }
+            /> < /
+            div > <
+            /Router> < /
+            div > );
     }
 }
 
